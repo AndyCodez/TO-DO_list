20dde8ad86d1e8f3a73ed332fe0ec2022b7628d8
AndyCodez <andystudy4@gmail.com>
2016-07-28 14:14:32 +0300

    Add named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  
+  root 'static_pages#home'
+  get 'signup' => 'users#new'
 
-  get 'static_pages/home'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
