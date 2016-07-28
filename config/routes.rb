62f8b3f3ebf1b4df2496eab31d6384719e6f0126
AndyCodez <andystudy4@gmail.com>
2016-07-28 16:49:59 +0300

    Generate sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   
   root 'static_pages#home'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
 
   resources :users
-
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
