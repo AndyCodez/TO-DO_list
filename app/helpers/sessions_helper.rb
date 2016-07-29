6d8048507df08cc4d1de4c5145e142e646ebc1c6
AndyCodez <andystudy4@gmail.com>
2016-07-29 11:18:47 +0300

    Logout user if logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,20 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  #Returns the current logged in user if any
+  def current_user
+    #Retrieves the stored session[:user_id], by finding the user in the db
+    #corresponding to the session id
+    @current_user ||= User.find_by(id: session[:user_id]) 
+  end
+
+  #Returns true if user is logged in
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
   resources :sessions
