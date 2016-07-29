5ee5b7d0404aff3179548d891cbc1306329c3d1f
AndyCodez <andystudy4@gmail.com>
2016-07-29 09:34:32 +0300

    Validate name email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  before_save :downcase_email
+  REGEX_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }, 
+            format: { with: REGEX_EMAIL },
+            uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
   has_secure_password
+
+  private
+
+    #Converts email to all lowercase
+    def downcase_email
+      self.email = email.downcase
+    end
 end
