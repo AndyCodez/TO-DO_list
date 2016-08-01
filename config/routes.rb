3b5b2b23785153d743ac7940ef0da3d94eca7dc4
AndyCodez <andystudy4@gmail.com>
2016-08-01 18:29:38 +0300

    Link to lissts path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   
-  get 'lists/new'
-
-  get 'lists/index'
-
-  get 'lists/show'
-
   root 'static_pages#home'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
@@ -14,5 +8,6 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
   resources :account_activations, only: [:edit]
+  resources :lists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
