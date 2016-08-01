764f8254e6a1e88fd5ab354baa600b21b66e8a00
AndyCodez <andystudy4@gmail.com>
2016-08-01 19:22:18 +0300

    Create relationship between user and lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,29 @@
 class ListsController < ApplicationController
 
   def new
+    @list = List.new
+  end
+
+  def create
+    @list = current_user.lists.build(list_params)
+    if @list.save
+      flash[:success] = "List successfully created!"
+      redirect_to lists_path
+    else
+      flash[:danger] = "Something went wrong, please try adding a list again."
+      render 'new'
+    end
   end
 
   def index
+    @lists = current_user.lists
   end
 
   def show
   end
+
+  private
+    def list_params
+      params.require(:list).permit(:title)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,11 @@ ActiveRecord::Schema.define(version: 20160801152352) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_lists_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
