edad21af6b4f5ae68f7d9957f6adebee6d03987c
AndyCodez <andystudy4@gmail.com>
2016-08-02 09:27:21 +0300

    Log in user on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       @user.send_activation_email
       flash[:success] = "Please check your email to activate your account."
       redirect_to @user
