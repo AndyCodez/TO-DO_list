11a3a250adecbd56825753f217c8aaa6e4e73a44
AndyCodez <andystudy4@gmail.com>
2016-08-02 11:57:13 +0300

    Create new card related to the particular list

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,23 @@
 class CardsController < ApplicationController
   def new
+    @list = List.find_by(id: params[:list_id])
     @card = Card.new
   end
+
+  def create
+    @list = List.find_by(id: params[:list_id])
+    @card = @list.cards.build(card_params)
+    if @card.save
+      flash[:success] = "Card added!"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Something went wrong, please try again."
+      render 'new'
+    end
+  end
+
+  private
+    def card_params
+      params.require(:card).permit(:title, :description)
+    end
 end
