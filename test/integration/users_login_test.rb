a50333f7cbb83797b55d2ff087cdc54993ae2777
AndyCodez <andystudy4@gmail.com>
2016-08-09 23:51:53 +0300

    Test login with valid  info and logout

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -11,11 +11,31 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     assert_template 'sessions/new'
     post sessions_path, params: { 
-      session: { username: "", password: " "  } 
+      session: { name: "", password: " "  } 
     }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "login with valid information followed by logout" do
+    get login_path
+    post sessions_path, params: {
+      session: { name: @user.name, password: 'password'   }
+
+    }
+    assert is_logged_in?
+    assert_redirected_to lists_path
+    follow_redirect!
+    assert_template 'lists/index'
+    assert_select "a[href=?]", logout_path
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    #simulates a user clicking logout in a second window
+    delete logout_path
+    follow_redirect!
+    assert_select "a[href=?]", logout_path, count: 0
   end
 end
