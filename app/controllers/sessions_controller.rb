b1884ff698b35ccd99acc9850665677a969a3991
AndyCodez <andystudy4@gmail.com>
2016-08-10 00:12:00 +0300

    Test login 2 days after signup without activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to lists_path
+      if user.created_at > 2.days && !user.activated?
+        #Deny full access to app functionality
+        flash[:danger] = "Please check your email and activate your account."
+        redirect_to root_url
+      else
+        #Allow access
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_to lists_path
+      end
     else
       flash.now[:danger] = "Invalid name/password combination."
       render 'new'
