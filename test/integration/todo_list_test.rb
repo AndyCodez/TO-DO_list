c149435734a98b905c55a5d750ed56f58ef6c229
AndyCodez <andystudy4@gmail.com>
2016-08-10 21:49:01 +0300

    Test user should be able to add move cards between lists

--- test/integration/todo_list_test.rb
+++ test/integration/todo_list_test.rb
@@ -20,7 +20,7 @@ class TodoListTest < ActionDispatch::IntegrationTest
     assert_equal 'List successfully created!', flash[:success]
   end
 
-  test "user should be able to add card to a list" do
+  test "user should be able to add card to a list and move card between lists" do
     log_in_as @user
     assert is_logged_in?
     get lists_path
@@ -34,9 +34,12 @@ class TodoListTest < ActionDispatch::IntegrationTest
     assert_equal 'Card added!', flash[:success]
     #Move cards between lists
     get move_card_path(card_id: @card.id) 
-    get choose_list_path(card_id: @card.id, list_id: @other_list.id) 
+    assert_difference "@list.cards.count", -1 do
+      get choose_list_path(card_id: @card.id, list_id: @other_list.id) 
+    end
     assert_redirected_to list_path(id: @other_list.id)
     assert_not flash.empty?
     assert_equal 'Card successfully moved!', flash[:success]
+    
   end
 end
