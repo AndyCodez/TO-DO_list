6d1f9de8fd16f7404a739314fa139845c2e3b869
AndyCodez <andystudy4@gmail.com>
2016-08-10 14:44:13 +0300

    Use Ajax to update lists path even when js is disabled.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,17 @@
 class ListsController < ApplicationController
 
+  skip_before_filter :verify_authenticity_token, :only => :create
+
   def create
     @list = current_user.lists.build(list_params)
     if @list.save
       flash[:success] = "List successfully created!"
-      redirect_to lists_path
+      respond_to do |format|
+        #Executes if js is disabled on browser
+        format.html { redirect_to lists_path }
+        #Executes if js is enabled on browser
+        format.js { redirect_to lists_path }
+      end
     else
       flash[:danger] = "Something went wrong, please try adding a list again."
       render 'new'
