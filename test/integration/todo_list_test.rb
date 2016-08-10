c153044ca4cb6e9454b8f609af5933b477ef9647
AndyCodez <andystudy4@gmail.com>
2016-08-10 20:52:19 +0300

    Test user should be able to create a list

--- test/integration/todo_list_test.rb
+++ test/integration/todo_list_test.rb
@@ -2,7 +2,19 @@ require 'test_helper'
 
 class TodoListTest < ActionDispatch::IntegrationTest
 
-  test "user should be able to add card to list" do
-    
+  def setup
+    @user = users(:makatunga)
+  end
+
+  test "user should be able to create a list" do
+    log_in_as  @user
+    assert is_logged_in?
+    get lists_path
+    assert_difference "List.count", 1 do
+      post lists_path, params:{ list: { title: "My List Title" } } 
+    end
+    follow_redirect!
+    assert_not flash.empty?
+    assert_template 'lists/index'
   end
 end
